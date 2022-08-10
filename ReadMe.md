9f7d098b2979aebb4732cacd9dfe2a773c9973e9
Stanley Festus <stanley.festus@spatineo.com>
2022-08-10 12:22:11 +0300

    first package version commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# using rollup as a tool to bundle javascript files
+# we don't typescript to bundle javascript files
+# typescript only bundles type files
+# rollup takes care of javascript files
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+interface ButtonProps {
+  text: string;
+}
+
+const Button = (props: ButtonProps) => {
+  const { text } = props;
+
+  return (
+    <button>
+      {text}
+    </button>
+  )
+}
+
+export default Button;
